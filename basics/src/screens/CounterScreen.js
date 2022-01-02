5e4c8be1f09af568af2fb4cc5bcdef606fdf085e
Debasish Sarker <debasishsarker36@gmail.com>
2022-01-02 21:14:46 +0600

    counter app working using react hook useState

--- basics/src/screens/CounterScreen.js
+++ basics/src/screens/CounterScreen.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, StyleSheet, Button} from "react-native";
 
+
 const CounterScreen = () => {
+    const [counter, setCounter] = useState(0);
     return (
         <View>
-            <Text>Counter Screen</Text>
+            <Button 
+                title="Increase"
+                onPress={ ()=> {
+                    setCounter(counter + 1);
+                }}>
+            </Button>
+            <Text style={styles.text}></Text>
+
+            <Button
+                title="Decrease"
+                onPress={() => {
+                    setCounter(counter - 1);
+                }}>
+
+            </Button>
+
+
+            <Text style={styles.text}>Count : {counter} </Text>
         </View>
     );
 };
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+    text:{
+        marginTop:30,
+        textAlign:"center",
+        fontSize:30
+    }
 });
 
 export default CounterScreen;
