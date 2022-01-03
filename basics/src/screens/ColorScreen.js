ce452057330547fd97c885117af41c1d8df5d445
Debasish Sarker <debasishsarker36@gmail.com>
2022-01-03 22:56:05 +0600

    added colored box

--- basics/src/screens/ColorScreen.js
+++ basics/src/screens/ColorScreen.js
@@ -1,12 +1,29 @@
 import React from "react";
 import {View, Button, StyleSheet} from "react-native";
 
+
 const ColorScreen = () => {
     return(
         <View>
             <Button title="Add Color"></Button>
-        </View>
-    )
+            <View 
+                style= {
+                    {
+                        height: 100,
+                        width: 100,
+                        backgroundColor: 'rgb(128,110,80)'
+                }}/>
+
+            <View 
+                style= {
+                    {
+                        height: 100,
+                        width: 100,
+                        backgroundColor: 'rgb(50,90,125)'
+                }}/>
+
+        </View>    
+    );
 }
 
 const styles = StyleSheet.create({});
