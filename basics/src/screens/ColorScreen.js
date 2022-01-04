19b35e661785b06a1766d117a8bbd907279ff69e
Debasish Sarker <debasishsarker36@gmail.com>
2022-01-04 23:22:08 +0600

    generating random colours

--- basics/src/screens/ColorScreen.js
+++ basics/src/screens/ColorScreen.js
@@ -11,7 +11,7 @@ const ColorScreen = () => {
                     {
                         height: 100,
                         width: 100,
-                        backgroundColor: 'rgb(128,110,80)'
+                        backgroundColor: randomRgb()
                 }}/>
 
             <View 
@@ -19,12 +19,21 @@ const ColorScreen = () => {
                     {
                         height: 100,
                         width: 100,
-                        backgroundColor: 'rgb(50,90,125)'
+                        backgroundColor: randomRgb()
                 }}/>
-
         </View>    
     );
-}
+};
+
+const randomRgb = () =>{
+    const red = Math.floor(Math.random()*256);
+    const green = Math.floor(Math.random()*256);
+    const blue = Math.floor(Math.random()*256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+};
+
+
 
 const styles = StyleSheet.create({});
 
